0cad2c0cc9e003e8ae93163bccc2a4cea850e51f
svenvbeek2001 <vanbeek-sven@live.nl>
2019-04-25 14:58:15 +0200

    Create productPage.php
    productPage gemaakt en aangepast
    
    mobile header

---
+++ UI-kit/productPage.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+        <title>EenmaalAndermaal</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/uikit.min.css" />
+        <link rel="stylesheet" href="css/style.css" />
+        <script src="js/uikit.min.js"></script>
+        <script src="js/uikit-icons.min.js"></script>
+</head>
+
+<body>
+<div class="uk-hidden@s">
+<div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky; bottom: #transparent-sticky-navbar">
+    <nav class="uk-navbar-container" uk-navbar style="position: relative; z-index: 980;">
+       
+
+            <ul class="uk-navbar-nav">
+        
+                <li><a href="index.php" uk-icon="icon: triangle-left; ratio: 3"></a></li>
+                
+            </ul>
+
+       
+    </nav>
+</div>
+</div>
+
+</body>
+
+</html>
