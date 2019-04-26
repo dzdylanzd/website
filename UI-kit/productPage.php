b57abe1535f4d2c4068f90b57f1cace315dcb0d1
svenvbeek2001 <vanbeek-sven@live.nl>
2019-04-26 12:06:35 +0200

    aanpassingen productpage borders

--- UI-kit/productPage.php
+++ UI-kit/productPage.php
@@ -2,69 +2,79 @@
 <html>
 
 <head>
-        <title>EenmaalAndermaal</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="css/uikit.min.css" />
-        <link rel="stylesheet" href="css/style.css" />
-        <script src="js/uikit.min.js"></script>
-        <script src="js/uikit-icons.min.js"></script>
+    <title>EenmaalAndermaal</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/uikit.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <script src="js/uikit.min.js"></script>
+    <script src="js/uikit-icons.min.js"></script>
 </head>
 
 <body>
-<?php include 'includes\nav-L-M.php'; ?>
+    <?php include 'includes\nav-L-M.php'; ?>
 
 
-<div class="uk-hidden@s">
-<div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky; bottom: #transparent-sticky-navbar">
-    <nav class="uk-navbar-container" uk-navbar style="position: relative; z-index: 980;">
-       
+    <div class="uk-hidden@s">
+        <div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky; bottom: #transparent-sticky-navbar">
+            <nav class="uk-navbar-container" uk-navbar style="position: relative; z-index: 980;">
 
-            <ul class="uk-navbar-nav">
-        
-                <li><a href="index.php" uk-icon="icon: triangle-left; ratio: 3"></a></li>
-                
-            </ul>
 
-       
-    </nav>
-</div>
-</div>
+                <ul class="uk-navbar-nav">
 
-<div class="uk-flex">
-    <div class="uk-card uk-card-default uk-card-body uk-width-1-3">  <!-- presentatie ding -->
-    <div class="uk-position-relative uk-visible-toggle uk-light  uk-width-4-4" tabindex="-1" uk-slideshow>
+                    <li><a href="index.php" uk-icon="icon: triangle-left; ratio: 3"></a></li>
 
-        <ul class="uk-slideshow-items">
-            <li>
-                <img src="https://via.placeholder.com/150" alt="" uk-cover>
-            </li>
-            <li>
-                <img src="https://via.placeholder.com/350" alt="" uk-cover>
-            </li>
-            <li>
-                <img src="https://via.placeholder.com/450" alt="" uk-cover>
-            </li>
-            <li>
-                <img src="https://via.placeholder.com/950" alt="" uk-cover>
-            </li>
-        </ul>
+                </ul>
 
-        <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>
-        <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slideshow-item="next"></a>
 
+            </nav>
+        </div>
     </div>
 
-    <div class="imagePrevieuw uk-flex  ">
-        <img src="https://via.placeholder.com/150" class="uk-width-1-4 " alt="D" onclick="UIkit.slideshow('.uk-slideshow').show(0);">
-        <img src="https://via.placeholder.com/350" class="uk-width-1-4 "  alt="D" onclick="UIkit.slideshow('.uk-slideshow').show(1);">
-        <img src="https://via.placeholder.com/450" class="uk-width-1-4 " alt="D" onclick="UIkit.slideshow('.uk-slideshow').show(2);">
-        <img src="https://via.placeholder.com/950" class="uk-width-1-4 "  alt="D" onclick="UIkit.slideshow('.uk-slideshow').show(3);">
+    <div class="uk-flex">
+        <div class="uk-width-1-3 Card-Empty">
+            <!-- presentatie ding -->
+            <div class="uk-position-relative uk-visible-toggle uk-light  uk-width-4-4	uk-margin-bottom" tabindex="-1" uk-slideshow>
 
-    </div></div>
-    <div class="streepje"></div>
-    <div class="uk-card uk-card-default uk-card-body uk-width-2-3">Item 2</div>
-</div>
+                <ul class="uk-slideshow-items ">
+                    <li class="Image-Border">
+                        <img src="https://via.placeholder.com/150" alt="" uk-cover>
+                    </li>
+                    <li class="Image-Border">
+                        <img  src="https://via.placeholder.com/350" alt="" uk-cover>
+                    </li>
+                    <li class="Image-Border">
+                        <img src="https://via.placeholder.com/450" alt="" uk-cover>
+                    </li>
+                    <li class="Image-Border">
+                        <img src="https://via.placeholder.com/950" alt="" uk-cover>
+                    </li>
+                </ul>
+
+                <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>
+                <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slideshow-item="next"></a>
+
+            </div>
+
+            <div class="imagePrevieuw uk-flex">
+                <img src="https://via.placeholder.com/150" class="uk-width-1-4 " alt="D" onclick="UIkit.slideshow('.uk-slideshow').show(0);">
+                <img src="https://via.placeholder.com/350" class="uk-width-1-4 " alt="D" onclick="UIkit.slideshow('.uk-slideshow').show(1);">
+                <img src="https://via.placeholder.com/450" class="uk-width-1-4 " alt="D" onclick="UIkit.slideshow('.uk-slideshow').show(2);">
+                <img src="https://via.placeholder.com/950" class="uk-width-1-4 " alt="D" onclick="UIkit.slideshow('.uk-slideshow').show(3);">
+
+            </div>
+        </div>
+        <div class="Vertical_Line"></div>
+        <div class="uk-width-2-3 Card-Empty">
+            <div class="uk-flex Bieding">
+                <div class="uk-width-1-2">
+                    <h1>tekstjes</h1>
+                </div>
+                <div class="uk-width-1-2">
+                    <h1>tekstjejhkgs</h1>
+                </div>
+            </div>
+        </div>
 
 </body>
 
