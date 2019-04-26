34e0848b214a5d7c334a26228b0f4dc150c568e2
svenvbeek2001 <vanbeek-sven@live.nl>
2019-04-26 10:39:48 +0200

    added box in productpage

--- UI-kit/productPage.php
+++ UI-kit/productPage.php
@@ -12,6 +12,9 @@
 </head>
 
 <body>
+<?php include 'includes\nav-L-M.php'; ?>
+
+
 <div class="uk-hidden@s">
 <div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky; bottom: #transparent-sticky-navbar">
     <nav class="uk-navbar-container" uk-navbar style="position: relative; z-index: 980;">
@@ -26,6 +29,12 @@
        
     </nav>
 </div>
+</div>
+
+<div class="uk-flex">
+    <div class="uk-card uk-card-default uk-card-body uk-width-1-4">Item 1</div>
+    <div class="streepje"></div>
+    <div class="uk-card uk-card-default uk-card-body uk-width-3-4">Item 2</div>
 </div>
 
 </body>
