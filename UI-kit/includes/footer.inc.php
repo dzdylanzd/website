931d9a433e684d3373e64e5c91f4572d0cf3a878
yunglook <hiddekoomen@gmail.com>
2019-05-06 10:55:36 +0200

    footer yo

---
+++ UI-kit/includes/footer.inc.php
@@ -0,0 +1,8 @@
+<?php
+echo <<<FOOTER
+<footer>
+    iConcepts is niet aansprakelijk voor (gevolg)schade die voortkomt uit het gebruik van deze site, dan wel uit fouten of ontbrekende functionaliteiten op deze site.
+    <br>Copyright © 2019 iConcepts. Alle rechten voorbehouden.
+</footer>
+FOOTER;
+?>
