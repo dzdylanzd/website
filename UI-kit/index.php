16075f935a4d6de410a27daf84e8bc4fd08bfa75
dylan <dylanzweers@gmail.com>
2019-05-06 15:08:24 +0200

    d
    d

--- UI-kit/index.php
+++ UI-kit/index.php
@@ -15,92 +15,94 @@
     <?php include 'includes\nav-L-M.php'; ?>
     <?php header('Refresh: 10'); ?>
     <div class="page-container">
-    <div class="content-wrap">
+        <div class="content-wrap">
 
 
 
 
-    <!-- navigatie balk S -->
-    <div class="uk-hidden@s">
-        <nav class="uk-navbar-container uk-flex-center uk-flex-column" uk-navbar>
+            <!-- navigatie balk S -->
+            <div class="uk-hidden@s">
+                <nav class="uk-navbar-container uk-flex-center uk-flex-column" uk-navbar>
 
-            <div class="uk-navbar-nav  uk-flex-center">
-                <a class=" uk-logo uk-navbar-item " href="#"><img src="media\logo.png" alt="logo" width=100em></a>
+                    <div class="uk-navbar-nav  uk-flex-center">
+                        <a class=" uk-logo uk-navbar-item " href="#"><img src="media\logo.png" alt="logo" width=100em></a>
 
-            </div>
+                    </div>
 
 
-            <div class="uk-navbar-nav  uk-flex-center">
-                <div class="uk-navbar-item ">
-                    <form action="productpage.php">
+                    <div class="uk-navbar-nav  uk-flex-center">
+                        <div class="uk-navbar-item ">
+                            <form action="productpage.php">
 
-                        <div class="uk-inline">
-                            <button class="uk-form-icon uk-form-icon-flip" uk-icon="icon: search" type="Submit"></button>
-                            <input class="uk-input" type="text" name="search" placeholder="Waar bent u naar op zoek?">
+                                <div class="uk-inline">
+                                    <button class="uk-form-icon uk-form-icon-flip" uk-icon="icon: search" type="Submit"></button>
+                                    <input class="uk-input" type="text" name="search" placeholder="Waar bent u naar op zoek?">
+                                </div>
+                            </form>
+                            <a class="uk-margin-left" href="index.php" uk-icon="icon: user"></a>
                         </div>
-                    </form>
-                    <a class="uk-margin-left" href="index.php" uk-icon="icon: user"></a>
+
+                    </div>
+
+
+
+
+                </nav>
+            </div>
+
+            <div class="uk-flex">
+                <div class="uk-width-1-5">
+                    <div class="scrollbox">
+                        Efficient honorificabilitudinitatibus
+                        cross-media information without floccinaucinihilipilification cross-media value. Quickly maximize timely deliverables for real-time schemas plenipotentiary.
+                        Efficient honorificabilitudinitatibus
+                        cross-media information without floccinaucinihilipilification cross-media value. Quickly maximize timely deliverables for real-time schemas plenipotentiary.
+                        Efficient honorificabilitudinitatibus
+                        cross-media information without floccinaucinihilipilification cross-media value. Quickly maximize timely deliverables for real-time schemas plenipotentiary.
+                    </div>
+
+
+                    <?php require_once('includes\catogorie _nav.php'); ?>
+
                 </div>
+                <div class="uk-width-4-5">Item 2
 
-            </div>
+                    <?php
+                    if (isset($_GET["root"])) {
+                        include 'includes/display_product.php';
+                        $stmt = $dbh->prepare("SELECT * from Categorieen where ID = ?");
+                        if ($stmt->execute(array($_GET["root"]))) {
+                            while ($row = $stmt->fetch()) {
+                                $TITELS = $dbh->prepare("SELECT Titel from items where Categorie = ?");
+                                if ($TITELS->execute(array($row["ID"]))) {
+                                    $row2 = $TITELS->fetch();
+                                    if ($row2  > 0) {
+                                        echo "<br><br><br> <h1> $row[Name]</h1> <br> ";
+                                        while ($row2 = $TITELS->fetch()) {
+                                            echo "$row2[Titel] <br>";
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        $stmt = $dbh->prepare("SELECT * from Categorieen where Parent = ?");
 
-
-
-
-        </nav>
-    </div>
-
-    <div class="uk-flex">
-        <div class="uk-width-1-5">
-            <div class="scrollbox">
-                Efficient honorificabilitudinitatibus
-                cross-media information without floccinaucinihilipilification cross-media value. Quickly maximize timely deliverables for real-time schemas plenipotentiary.
-                Efficient honorificabilitudinitatibus
-                cross-media information without floccinaucinihilipilification cross-media value. Quickly maximize timely deliverables for real-time schemas plenipotentiary.
-                Efficient honorificabilitudinitatibus
-                cross-media information without floccinaucinihilipilification cross-media value. Quickly maximize timely deliverables for real-time schemas plenipotentiary.
-            </div>
-
-
-            <?php require_once('includes\catogorie _nav.php'); ?>
-
-        </div>
-        <div class="uk-width-4-5">Item 2
-
-            <?php
-            if(isset($_GET["root"])){
-            include 'includes/display_product.php';
-            $stmt = $dbh->prepare("SELECT * from Categorieen where ID = ?");
-            if ($stmt->execute(array($_GET["root"]))) {
-                while ($row = $stmt->fetch()) {
-                    $TITELS = $dbh->prepare("SELECT Titel from items where Categorie = ?");
-                    if ($TITELS->execute(array($row["ID"]))) {
-                        $row2 = $TITELS->fetch();
-                        if ($row2  > 0) {
-                            echo "<br><br><br> <h1> $row[Name]</h1> <br> ";
-                            while ($row2 = $TITELS->fetch()) {
-                                echo "$row2[Titel] <br>";
+                        if ($stmt->execute(array($_GET["root"]))) {
+                            while ($row = $stmt->fetch()) {
+                                echo "<h1> $row[Name] </h1>";
+                                if (displayCatogorie($row["ID"], $dbh)) { }
                             }
                         }
                     }
-                }
-            }
-            $stmt = $dbh->prepare("SELECT * from Categorieen where Parent = ?");
-
-            if ($stmt->execute(array($_GET["root"]))) {
-                while ($row = $stmt->fetch()) {
-                    echo "<h1> $row[Name] </h1>";
-                    if (displayCatogorie($row["ID"], $dbh)) { }
-                }
-            }
-        }
-            ?>
+                    ?>
 
 
-        </div>
+                </div>
 
-        </div>
-        </div>
+            </div>
+        </div></div>
         <?php include 'includes/footer.inc.php'; ?>
-    </body>
-    </html>
+                
+</body>
+
+</html>
