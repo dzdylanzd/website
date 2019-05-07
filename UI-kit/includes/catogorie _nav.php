df18e86ad1d2babeaf60585a55c6665392462b6c
dylan <dylanzweers@gmail.com>
2019-05-07 13:37:49 +0200

    homepage af
    home page af

--- UI-kit/includes/catogorie _nav.php
+++ UI-kit/includes/catogorie _nav.php
@@ -4,9 +4,9 @@
     $stmt = $dbh->prepare("SELECT * from Categorieen where Parent = ?");
 
     if ($stmt->execute(array(-1))) {
-      echo "<ul>";
+      echo "<ul class=\"noDots\" >";
       while ($row = $stmt->fetch()) {
-        echo "<li> <a href=\"categorieen.php?root=$row[ID]\">  $row[Name] </a> </li>  ";
+        echo "<li> <a class=\"uk-link-heading\" href=\"categorieen.php?root=$row[ID]\">  $row[Name] </a> </li>  ";
       }
       echo "</ul>";
     }
@@ -42,14 +42,14 @@
 
 
 
-    echo "<ul>";
+    echo "<ul class=\"noDots\">";
     if ($_GET["root"] != -1) {
       $stmt = $dbh->prepare("SELECT * from Categorieen where ID = ?");
 
       if ($stmt->execute(array($_GET["root"]))) {
 
         while ($row = $stmt->fetch()) {
-          echo "<li> <a href=\"categorieen.php?root=$row[Parent]\">  terug </a> </li>  ";
+          echo "<li> <a class=\"uk-link-heading\" href=\"categorieen.php?root=$row[Parent]\">  terug </a> </li>  ";
         }
       }
     }
@@ -60,16 +60,16 @@
       if ($row = $stmt->fetch() > 0) {
 
         while ($row = $stmt->fetch()) {
-          echo "<li> <a href=\"categorieen.php?root=$row[ID]\">  $row[Name] </a> </li>  ";
+          echo "<li> <a class=\"uk-link-heading\" href=\"categorieen.php?root=$row[ID]\">  $row[Name] </a> </li>  ";
         }
-        echo "</ul>";
+        echo "</ul >";
       } else {
         $stmt = $dbh->prepare("SELECT * from Categorieen where ID = ?");
 
         if ($stmt->execute(array($_GET["root"]))) {
 
           while ($row = $stmt->fetch()) {
-            echo "<li> <a href=\"categorieen.php?root=$row[Parent]\">  $row[Name] </a> </li>  ";
+            echo "<li> <a class=\"uk-link-heading\" href=\"categorieen.php?root=$row[Parent]\">  $row[Name] </a> </li>  ";
           }
           echo "</ul>";
         }
