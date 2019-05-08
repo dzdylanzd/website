9ebb4ad672386ae797ff30004135cc6a32e565dd
dylan <dylanzweers@gmail.com>
2019-05-08 11:33:26 +0200

    Revert "Revert "tranquillo""
    This reverts commit 2e73ad828bfdeb3c3a8346e91c07e5ba0f45b100.

--- UI-kit/includes/display_product.php
+++ UI-kit/includes/display_product.php
@@ -1,6 +1,6 @@
 <?php
 
-function displayCatogorie($nummer, $dbh){
+function displayCategorie($nummer, $dbh) {
     if($nummer = -1){
         return false;
     }
@@ -54,47 +54,45 @@ function displayCatogorie($nummer, $dbh){
             SELECT id FROM Categorieen WHERE Parent= $nummer or id = $nummer ))))
             )";
 
-$sth = $dbh->prepare($sql);
-if($sth->execute(array())){
-echo'<div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slider="clsActivated: uk-transition-active; ">
+            $sth = $dbh->prepare($sql);
+            if($sth->execute(array())){
+            echo'<div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slider="clsActivated: uk-transition-active; ">
 
-<ul class="uk-slider-items uk-grid">';
-while ($alles = $sth->fetch()) {
-    $sqlImage = "SELECT TOP 1 * FROM Illustraties WHERE ItemID = ? ";
-    $sthImage = $dbh->prepare($sqlImage);
-if($sthImage->execute(array($alles["ID"]))){
-    $image = $sthImage->fetch();
-    $titel = substr($alles["Titel"],6);
-}
-if($alles["Valuta"] = "EUR"){
-    $valutaTeken = "€";
-}else if($alles["Valuta"] = "USD"){
-    $valutaTeken = "$";
-}else if($alles["Valuta"] = "GBP"){
-    $valutaTeken = "£";
-}else if($alles["Valuta"] = "AUD"){
-    $valutaTeken = "\$A";
-}
-    echo "
-            <li class=\"uk-width-1-4@l uk-width-1-3@m uk-width-1-2@s\">
-            <div class=\"uk-panel\">
-               <a href=\"productPage.php?ID=$alles[ID]\" > <img  class=\"image-square\" src=\"http://iproject5.icasites.nl/thumbnails/$alles[Thumbnail]\" alt=\"\"> </a>
-                <div class=\"uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom\">
-                    <h3 class=\"uk-margin-remove\">";
-                    echo substr($alles["Titel"],0,10);
-                    echo "... </h3>
-                    <p class=\"uk-margin-remove\"> $valutaTeken $alles[Prijs]</p>
+            <ul class="uk-slider-items uk-grid">';
+            while ($alles = $sth->fetch()) {
+                $sqlImage = "SELECT TOP 1 * FROM Illustraties WHERE ItemID = ? ";
+                $sthImage = $dbh->prepare($sqlImage);
+                if($sthImage->execute(array($alles["ID"]))){
+                    $image = $sthImage->fetch();
+                    $titel = substr($alles["Titel"],6);
+                }
+                if($alles["Valuta"] = "EUR"){
+                    $valutaTeken = "€";
+                }else if($alles["Valuta"] = "USD"){
+                    $valutaTeken = "$";
+                }else if($alles["Valuta"] = "GBP"){
+                    $valutaTeken = "£";
+                }else if($alles["Valuta"] = "AUD"){
+                    $valutaTeken = "\$A";
+                }
+                    echo "
+                <li class=\"uk-width-1-4@l uk-width-1-3@m uk-width-1-2@s\">
+                <div class=\"uk-panel\">
+                <a href=\"productPage.php?ID=$alles[ID]\" > <img  class=\"image-square\" src=\"http://iproject5.icasites.nl/thumbnails/$alles[Thumbnail]\" alt=\"\"> </a>
+                    <div class=\"uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom\">
+                        <h3 class=\"uk-margin-remove\">";
+                        echo substr($alles["Titel"],0,10);
+                        echo "... </h3>
+                        <p class=\"uk-margin-remove\"> $valutaTeken $alles[Prijs]</p>
+                    </div>
                 </div>
-            </div>
-        </li>";
-}
-echo'</ul>
+                </li>";
+            }
+        echo'</ul>
 
-<a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
-<a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
+        <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+        <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
 
-</div>';
-}
-
-
+        </div>';
+    }
 }
