583b831ecb58d7b110c4965764afe07b3eaebfb0
dylan <dylanzweers@gmail.com>
2019-05-08 11:07:57 +0200

    Update categorie _nav.php

--- UI-kit/includes/categorie _nav.php
+++ UI-kit/includes/categorie _nav.php
@@ -1,39 +1,4 @@
- <?php
-  require_once('includes\database.php');
-  if (!isset($_GET["root"])) {
-    $stmt = $dbh->prepare("SELECT * from Categorieen where Parent = ?");
 
-    if ($stmt->execute(array(-1))) {
-      echo "<ul class=\"noDots\">";
-      while ($row = $stmt->fetch()) {
-        echo "<li> <a class=\"uk-link-heading\" href=\"categorieen.php?root=$row[ID]\">  $row[Name] </a> </li>";
-      }
-      echo "</ul>";
-    }
-  } else {
-
-    $stmt = $dbh->prepare("SELECT * from Categorieen where ID = ?");
-
-    if ($stmt->execute(array($_GET["root"]))) {
-
-      while ($row = $stmt->fetch()) {
-        if($row["ID"] != -1){
-        $text =  "<a class=\"uk-link-heading\" href=\"categorieen.php?root=$row[ID]\">  $row[Name] </a>";
-        $parent = $row["Parent"];
-       
-          while($parent > 0){
-            $stmt2 = $dbh->prepare("SELECT * from Categorieen where ID = ?");
-
-          if ($stmt2->execute(array($parent))) {
-            while ($row2 = $stmt2->fetch()) { 
-             $text =  " <a class=\"uk-link-heading\" href=\"categorieen.php?root=$row2[ID]\">  $row2[Name] </a> /  $text";
-              $parent = $row2["Parent"];
-              }
-            }
-          }
-        echo $text;
-        }        
-      }
     }
 
 
