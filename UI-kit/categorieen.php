c1876b54ab810afc64c1477ee68e697c4e9ea0f1
yunglook <hiddekoomen@gmail.com>
2019-05-09 09:50:53 +0200

    fix

--- UI-kit/categorieen.php
+++ UI-kit/categorieen.php
@@ -58,40 +58,36 @@
                         </div>
                         <div>
                         <h3> Staat</h3>
-                            <form class="FilterenStaat" action="categorieen.php" method="post">
-                                <input type="checkbox" name="nieuw" value="Nieuw"> Nieuw<br>
-                                <input type="checkbox" name="bijnaNieuw" value="bijnaNieuw"> Zo goed als nieuw<br>
-                                <input type="checkbox" name="gebruikt" value="Gebruikt"> Gebruikt<br>
-</form>
-                                <h3> Prijs</h3>
-                                <form action="categorieen.php" method="post">
-                                    <label for="prijsVan"> Van</label>
-                                    <input class="FilterenPrijs" type="text" name="prijs" id="prijsVan">
-                                    <label for="prijsTot"> Tot</label>
-                                    <input class="FilterenPrijs" type="text" name="prijs" id="prijsTot">
-</form>
-                                <h3> Locatie</h3>
-                                <form action="categorieen.php"  method="post">
-                                    <label for="afstand"> Binnen</label>
-                                    <select name="afstanden">
-                                        <option value="10km"> < 10 kilometer </option>
-                                        <option value="25km"> < 25 kilometer </option>
-                                        <option value="50km"> < 50 kilometer </option>
-                                        <option value="100km"> < 100 kilometer </option>
-                                        <option value="250km"> < 250 kilometer </option>
-                                        <option value="meer250km"> > 250 kilometer </option>
-</select>
-                                </form>
-</div>
+                        <form class="FilterenStaat" action="categorieen.php" method="post">
+                            <input type="checkbox" name="nieuw" value="Nieuw"> Nieuw<br>
+                            <input type="checkbox" name="bijnaNieuw" value="bijnaNieuw"> Zo goed als nieuw<br>
+                            <input type="checkbox" name="gebruikt" value="Gebruikt"> Gebruikt<br>
+                        </form>
+                        <h3> Prijs</h3>
+                        <form action="categorieen.php" method="post">
+                            <label for="prijsVan"> Van</label>
+                            <input class="FilterenPrijs" type="text" name="prijs" id="prijsVan">
+                            <label for="prijsTot"> Tot</label>
+                            <input class="FilterenPrijs" type="text" name="prijs" id="prijsTot">
+                        </form>
+                        <h3> Locatie</h3>
+                        <form action="categorieen.php"  method="post">
+                            <label for="afstand"> Binnen</label>
+                            <select name="afstanden">
+                                <option value="niks"> ... </option>
+                                <option value="10km"> < 10 kilometer </option>
+                                <option value="25km"> < 25 kilometer </option>
+                                <option value="50km"> < 50 kilometer </option>
+                                <option value="100km"> < 100 kilometer </option>
+                                <option value="250km"> < 250 kilometer </option>
+                                <option value="500km"> < 500 kilometer </option>
+                            </select>
+                        </form>
                     </div>
-
-
-
-
                 </div>
-                <div class="uk-width-5-5">
+            </div>
+                <div class="uk-width-3-4">
                     <?php
-                    
                     if (isset($_GET["root"])) {
                      
                         $sth = $dbh->prepare("SELECT * FROM Rubriek WHERE Rubrieknummer  = ? ");
@@ -112,7 +108,7 @@
                             while ($row = $sth->fetch()) {
                                 if($row["Rubrieknummer"] != -1){
                                 echo'<div class="ItemsSlider">';
-                                echo "<h2> $row[Rubrieknaam] </h2>";
+                                echo "<h3> $row[Rubrieknaam] </h3>";
                                 displayCategorie($row["Rubrieknummer"], $dbh,10);
                                echo' </div>';
                                 }
