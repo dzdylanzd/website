f1e82139179896d5b54594e83eb4cfceadf42ed3
dylan <dylanzweers@gmail.com>
2019-05-10 19:25:56 +0200

    d
    cf

--- UI-kit/productPage.php
+++ UI-kit/productPage.php
@@ -21,7 +21,7 @@
             <div class="uk-hidden@s">
                 <nav class="uk-navbar-container uk-flex-center uk-flex-column" uk-navbar>
                     <div class="uk-navbar-nav  uk-flex-center">
-                        <a class=" uk-logo uk-navbar-item " href="#"><img src="media\logo.png" alt="logo" width=100em></a>
+                        <a class=" uk-logo uk-navbar-item " href="index.php"><img src="media\logo.png" alt="logo" width=100em></a>
                     </div>
                     <div class="uk-navbar-nav  uk-flex-center">
                         <div class="uk-navbar-item ">
