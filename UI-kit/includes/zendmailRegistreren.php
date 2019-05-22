4b227e6464cb2a3a5e563a7b7c26d9e6b1f628d3
svenvbeek2001 <vanbeek-sven@live.nl>
2019-05-22 11:13:20 +0200

    Update zendmailRegistreren.php

--- UI-kit/includes/zendmailRegistreren.php
+++ UI-kit/includes/zendmailRegistreren.php
@@ -33,7 +33,8 @@ Beste meneer/mevrouw,<br>
 Bedankt dat u voor EenmaalAndermaal heeft gekozen.<br>
 U kunt nu beginnen met het bieden op veilingen!<br>
 Wilt u inloggen? Dit kan door op de inlogknop te klikken en uw gegevens in te vullen.<br>
-Uw gebruikersnaam is:  <strong>' . $gebruiksersnaam . '.
+Uw gebruikersnaam is:  <strong>' . $gebruiksersnaam . '.<br>
+U kunt ten alle tijden uw gegevens wijzigen op de mijn gegevens pagina.
 
 </strong>
 <br>
