0890e83b94f48405b9a1196cae6a0d117e432cfb
dylan <dylanzweers@gmail.com>
2019-05-23 15:04:11 +0200

    d
    d

--- UI-kit/includes/veiling-maken.inc.php
+++ UI-kit/includes/veiling-maken.inc.php
@@ -49,7 +49,7 @@ if ($sth = $dbh->prepare($sql)) {
   }
 }
 
-$sql = 'insert into Voorwerp(VoorwerpNummer,Titel,Beschrijving,StartPrijs,Betalingswijze, BetalingsInstructie,Plaatsnaam,Land,Looptijd,LooptijdBegin,Verzendkosten,Verkoper,LooptijdEinde,IsVeilingGesloten,Staat,Valuta,,VerzendInstructies)
+$sql = 'insert into Voorwerp(VoorwerpNummer,Titel,Beschrijving,StartPrijs,Betalingswijze, BetalingsInstructie,Plaatsnaam,Land,Looptijd,LooptijdBegin,Verzendkosten,Verkoper,LooptijdEinde,IsVeilingGesloten,Staat,Valuta,VerzendInstructies)
 values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)';
 $sqlImage = 'insert into Illustraties(VoorwerpNummer,Illustratiefile)
 values(?,?)';
