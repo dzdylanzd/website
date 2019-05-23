52ef0249917e8a7c8137623e78ba50875ddef045
svenvbeek2001 <vanbeek-sven@live.nl>
2019-05-23 15:20:20 +0200

    error

--- UI-kit/verkoperActiveren.php
+++ UI-kit/verkoperActiveren.php
@@ -19,20 +19,26 @@
         <div class="content-wrap">
 
             <div class="verkoper" uk-filter="target: .js-filter">
-                <h2> Activeren verkoopaccount </h2>
-                <p class="voorwaarden">Voer hier uw veificatiecode in die u per mail ontvangen heeft.</p>
-                <div class="verkoopbox">
-                    <h3>Activeren</h3>
-                    <form method="post" action="includes/verkoperActiveren.inc.php">
-                        <label for="verificatiecode">Verificatiecode</label><br>
-                        <input class="uk-input input-registratie" type="text" name="verificatiecode" id="verificatiecode"><br>
-                        <button name="verkoopaccountActiveren" type="submit" class="uk-button knop-lang">Verkoopaccount activeren</button>
-                    </form>
+                <?php if (isset($_GET["error"])) {
+                    if ($_GET["errorLogin"] == "leeg") {
+                        echo "<br> <p class=\"error\"> Gelieve alle velden in te vullen. </p>";
+                    }
+                    }
+                    ?>
+                    <h2> Activeren verkoopaccount </h2>
+                    <p class="voorwaarden">Voer hier uw veificatiecode in die u per mail ontvangen heeft.</p>
+                    <div class="verkoopbox">
+                        <h3>Activeren</h3>
+                        <form method="post" action="includes/verkoperActiveren.inc.php">
+                            <label for="verificatiecode">Verificatiecode</label><br>
+                            <input class="uk-input input-registratie" type="text" name="verificatiecode" id="verificatiecode"><br>
+                            <button name="verkoopaccountActiveren" type="submit" class="uk-button knop-lang">Verkoopaccount activeren</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <?php include 'includes/footer.inc.php'; ?>
-</body>
+        <?php include 'includes/footer.inc.php'; ?>
+    </body>
 
-</html>
+    </html>
