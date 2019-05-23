84e98276584e033e8cf0ce28dd7554f1fb4a120b
yunglook <hiddekoomen@gmail.com>
2019-05-23 16:09:22 +0200

    dikke jo

--- UI-kit/mijn-gegevens.php
+++ UI-kit/mijn-gegevens.php
@@ -89,20 +89,20 @@
                         <p class="mijngegevens">Achternaam: <?php echo $achternaam ?> </p><br>
                         <p class="mijngegevens">Geboortedatum: <?php echo $geboortedatum ?> </p><br>
 
-                 <?php       
-                $sql = 'SELECT Telefoonnummer FROM Gebruikerstelefoon WHERE gebruiker = ?';
-                if ($sth = $dbh->prepare($sql)) {
-                    if ($sth->execute(array($gebruikersnaam))) {
-                        while ($row = $sth->fetch()) {
-                            echo"<p class=\"mijngegevens\">Telefoonnummer:";
-                            echo" $row[Telefoonnummer]";
-                            echo"</p><br>";
+                        <?php
+                        $sql = 'SELECT Telefoonnummer FROM Gebruikerstelefoon WHERE gebruiker = ?';
+                        if ($sth = $dbh->prepare($sql)) {
+                            if ($sth->execute(array($gebruikersnaam))) {
+                                while ($row = $sth->fetch()) {
+                                    echo "<p class=\"mijngegevens\">Telefoonnummer:";
+                                    echo " $row[Telefoonnummer]";
+                                    echo "</p><br>";
+                                }
+                            }
                         }
-                    }
-                }
-                ?>
-                <p class="mijngegevens"> <a href="telefoonnummerToevoegen.php">telefoon nummer toevoegen</a></p>
-                 
+                        ?>
+                        <p class="mijngegevens"> <a href="telefoonnummerToevoegen.php">Telefoonnummer toevoegen</a></p>
+
                     </div>
                     <div class="registreerbox">
                         <h3>Adresgegevens</h3>
