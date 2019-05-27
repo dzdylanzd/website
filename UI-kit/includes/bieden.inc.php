73a69237905c357e34a58a10ba387f559fd3bd33
dylan <dylanzweers@gmail.com>
2019-05-27 09:47:30 +0200

    dd
    d

--- UI-kit/includes/bieden.inc.php
+++ UI-kit/includes/bieden.inc.php
@@ -15,6 +15,34 @@ if(!isset($_SESSION['userId'])){
 $sql = 'insert into bod(Voorwerp,BodBedrag,Gebruiker,BodDagTijd)
 Values (?,?,?,?)';
 
+
+$sql2 = "SELECT StartPrijs FROM Voorwerp WHERE VoorwerpNummer = ?";
+if ($sth = $dbh->prepare($sql2)) {
+    if ($sth->execute(array($_GET["ID"]))) {
+        while ($row = $sth->fetch()) {
+            $minimumVerhoging = $row['StartPrijs'];
+            $bod =  $minimumVerhoging;
+            if ($bod > 1 && $bod <= 50) {
+                $minimumVerhoging = $minimumVerhoging + 0.5;
+            } else if ($bod > 50 && $bod <= 500) {
+                $minimumVerhoging = $minimumVerhoging + 1;
+            } else if ($bod > 500 && $bod <= 1000) {
+                $minimumVerhoging =  $minimumVerhoging + 5;
+            } else if ($bod > 1000 && $bod <= 5000) {
+                $minimumVerhoging =  $bod + 10;
+            } else if ($bod >  5000) {
+                $minimumVerhoging = $minimumVerhoging  + 50;
+            }
+        }
+    }
+}
+
+if($bod <  $minimumVerhoging ){
+    header("location: $_SERVER[HTTP_REFERER]&error=teLaagBod");
+        exit();
+}
+
+
 $sql2 ="select isVeilingGesloten from Voorwerp where VoorwerpNummer = ?";
 $sth = $dbh->prepare($sql2);
 if($sth->execute(array($productid))){
