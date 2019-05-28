caf3a38add86010992307872b2a5ae6b52139028
dylan <dylanzweers@gmail.com>
2019-05-28 14:28:45 +0200

    d
    d

--- UI-kit/includes/verwijderAccount.inc.php
+++ UI-kit/includes/verwijderAccount.inc.php
@@ -1,8 +1,13 @@
 <?php
 session_start();
-
+require_once('database.php');
 $sql = 'delete Gebruiker where Gebruikersnaam = ?';
 
 $gebruiker =  $_SESSION['userId'];
 
-
+if ($sth = $dbh->prepare($sql)) {
+    if ($sth->execute(array($gebruiker))) {
+        header("location: logout.php");
+        exit();
+    }
+}
