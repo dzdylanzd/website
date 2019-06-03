d0c99da5207625044ed699a62c27c141e2e0b6e0
yunglook <hiddekoomen@gmail.com>
2019-06-03 12:33:09 +0200

    faq + aantal dingetjes

--- UI-kit/mijn-veilingen.php
+++ UI-kit/mijn-veilingen.php
@@ -22,7 +22,8 @@
 
             <!-- titel van pagina -->
             <div class="mijn-veilingen-titel">
-                <h2>Mijn veilingen <button onclick="window.location.href='veiling-Maken.php'" name="bevestigings-button" class="uk-button knop-nieuwe-veiling">Nieuwe veiling maken</button><h2>
+                <h2>Mijn veilingen <button onclick="window.location.href='veiling-Maken.php'" name="bevestigings-button" class="uk-button knop-nieuwe-veiling">Nieuwe veiling maken</button>
+                    <h2>
             </div>
 
             <!-- veilingen -->
@@ -39,6 +40,7 @@
                     $looptijd = '';
                     $thumbnail = '';
                     $huidigbod = '';
+                    $isVeilingGesloten = 0;
 
                     // Haal titel, looptijd en voorwerpnummer op
                     $sql = 'SELECT * FROM Voorwerp WHERE Verkoper = ?';
@@ -47,24 +49,25 @@
                             while ($row = $sth->fetch()) {
                                 $titel = $row['Titel'];
                                 $titel = substr($titel, 0, 30);
-                                
+
                                 $voorwerpnummer = $row['VoorwerpNummer'];
                                 $valuta = $row['Valuta'];
+                                $isVeilingGesloten = $row['IsVeilingGesloten'];
 
                                 // Schrijf valuta om in tekens
                                 switch ($valuta) {
-                                    case 'EUR': 
+                                    case 'EUR':
                                         $valuta = '€';
                                         break;
-                                    
+
                                     case 'GBP':
                                         $valuta = '£';
                                         break;
-                                    
+
                                     case 'AUD':
                                         $valuta = 'A$';
                                         break;
-                                    
+
                                     case 'CAD':
                                         $valuta = 'C$';
                                         break;
@@ -89,14 +92,14 @@
                                 if ($sthThumbnail = $dbh->prepare($sqlThumbnail)) {
                                     if ($sthThumbnail->execute(array($voorwerpnummer))) {
                                         while ($rowThumbnail = $sthThumbnail->fetch()) {
-                                            
 
-                                            if(strpos( $rowThumbnail['Thumbnailfile'],"img") !== false){
-                                                $rowThumbnail['Thumbnailfile'] = "http://iproject5.icasites.nl/thumbnails/".  $rowThumbnail['Thumbnailfile'];
-                                            }else{
+
+                                            if (strpos($rowThumbnail['Thumbnailfile'], "img") !== false) {
+                                                $rowThumbnail['Thumbnailfile'] = "http://iproject5.icasites.nl/thumbnails/" .  $rowThumbnail['Thumbnailfile'];
+                                            } else {
                                                 $rowThumbnail['Thumbnailfile'] = $rowThumbnail['Thumbnailfile'];
                                             }
-                                           
+
                                             $thumbnail = $rowThumbnail['Thumbnailfile'];
                                         }
                                     }
@@ -112,28 +115,32 @@
                                     }
                                 }
 
-                                echo '<div class=" veilingbox">';
+                                echo '<div class="veilingbox">';
                                 echo '<h3>' . $titel . '...</h3>';
-                                echo '<a href="productPage.php?ID='. $voorwerpnummer . '"><img class="mijn-veilingen-thumbnail" src="'.$thumbnail.'" alt="Thumbnail"></a><br>';
-                                echo "<h3 class=\"mijn-veilingen\"> Tijd resterend: <br>  
-                                        <div class=\"margin-left uk-grid-small uk-child-width-auto\" uk-grid uk-countdown=\"date:  $looptijd\">
-                                            <div>
-                                                <div class=\"countdown-getal-klein uk-countdown-number uk-countdown-days\"></div>
-                                            </div>
-                                            <div class=\"countdown-getal-klein uk-countdown-separator\">d</div>
-                                            <div>
-                                                <div class=\"countdown-getal-klein uk-countdown-number uk-countdown-hours\"></div>
-                                            </div>
-                                            <div class=\"countdown-getal-klein uk-countdown-separator\">u</div>
-                                            <div>
-                                                <div class=\"countdown-getal-klein uk-countdown-number uk-countdown-minutes\"></div>
-                                            </div>
-                                            <div class=\"countdown-getal-klein uk-countdown-separator\">m</div>
-                                            <div>
-                                                <div class=\"countdown-getal-klein uk-countdown-number uk-countdown-seconds\"></div>
-                                            </div>
-                                            <div class=\"countdown-getal-klein uk-countdown-separator\">s</div>
-                                        </div></h3>";
+                                echo '<a href="productPage.php?ID=' . $voorwerpnummer . '"><img class="mijn-veilingen-thumbnail" src="' . $thumbnail . '" alt="Thumbnail"></a><br>';
+                                if ($isVeilingGesloten == 0) {
+                                    echo '<h3 class="mijn-veilingen"> Tijd resterend: <br>  
+                                            <div class="margin-left uk-grid-small uk-child-width-auto" uk-grid uk-countdown="date:  $looptijd">
+                                                <div>
+                                                    <div class="countdown-getal-klein uk-countdown-number uk-countdown-daysf"></div>
+                                                </div>
+                                                <div class="countdown-getal-klein uk-countdown-separator">d</div>
+                                                <div>
+                                                    <div class="countdown-getal-klein uk-countdown-number uk-countdown-hours"></div>
+                                                </div>
+                                                <div class="countdown-getal-klein uk-countdown-separator">u</div>
+                                                <div>
+                                                    <div class="countdown-getal-klein uk-countdown-number uk-countdown-minutes"></div>
+                                                </div>
+                                                <div class="countdown-getal-klein uk-countdown-separator">m</div>
+                                                <div>
+                                                    <div class="countdown-getal-klein uk-countdown-number uk-countdown-seconds"></div>
+                                                </div>
+                                                <div class="countdown-getal-klein uk-countdown-separator">s</div>
+                                            </div></h3>';
+                                } else if ($isVeilingGesloten == 1) {
+                                    echo '<h3 class="mijn-veilingen errors"> Deze veiling is gesloten </h3>';
+                                }
                                 echo '<h3>Huidig bod: ' . $valuta . (double)$huidigbod;
                                 echo '</div></h3>';
                             }
