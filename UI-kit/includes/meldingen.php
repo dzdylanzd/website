3bb92d59dc3d9b948fc42042e580f73cae56ba2e
axelduif <49636806+axelduif@users.noreply.github.com>
2019-06-04 10:25:50 +0200

    dylans hoofdletterdingen

--- UI-kit/includes/meldingen.php
+++ UI-kit/includes/meldingen.php
@@ -1,19 +1,19 @@
 <?php
-
+echo $_SESSION['userId'];
 require_once('database.php');
-$sql = 'select * from meldingen where ontvanger = ? and ontvangen = 0';
-$sql2 = '	update meldingen
-set ontvangen = 1  where meldingId = ?';
+$sql = 'select * from Meldingen where Ontvanger = ? and Ontvangen = 0';
+$sql2 = '	update Meldingen
+set Ontvangen = 1  where MeldingID = ?';
 if(isset($_SESSION['userId'])){
 
 if ($sth = $dbh->prepare($sql)) {
     if ($sth->execute(array($_SESSION['userId']))) {      
       while ($alles = $sth->fetch()) {   
 
-echo "<script> UIkit.notification('$alles[bericht]', {pos: 'bottom-right'}); </script>";
+echo "<script> UIkit.notification('$alles[Bericht]', {pos: 'bottom-right'}); </script>";
 
 if ($sth2 = $dbh->prepare($sql2)) {
-    if ($sth2->execute(array($alles['meldingId'] ))) {   
+    if ($sth2->execute(array($alles['MeldingID'] ))) {   
     }
 }
 
